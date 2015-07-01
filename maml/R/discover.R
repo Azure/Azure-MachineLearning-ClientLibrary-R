9de2378cde104bd7eb76404f4bec806af6ddc848
Alex Wang <alexwang@college.harvard.edu>
2015-07-01 10:42:12 -0700

    First code commit
    Committed existing R code within package skeleton (maml):
    - discovery functions
    - basic consumption functions (old version of Ritika's)
    - publish function (work in progress)

---
+++ maml/R/discover.R
@@ -0,0 +1,116 @@
+#############################################################
+# API URLs
+#############################################################
+wsURL = "https://management-tm.azureml.net/workspaces/%s/webservices"
+wsURLdet = "https://management-tm.azureml.net/workspaces/%s/webservices/%s"
+epURL = "https://management-tm.azureml.net/workspaces/%s/webservices/%s/endpoints"
+epURLdet = "https://management-tm.azureml.net/workspaces/%s/webservices/%s/endpoints/%s"
+
+#############################################################
+# Framework for making an HTTP request to the URL specified
+# returns a list of lists, so that the elements can be accessed
+# via double bracket notation, e.g. results[[1]]
+#############################################################
+getFramework <- function(tUrl, authToken) {
+  # Collectors for API response
+  h = RCurl::basicTextGatherer()
+  hdr = RCurl::basicTextGatherer()
+
+  # Accept SSL certificates issued by public Certificate Authorities
+  options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+
+  # Craft request header and execute
+  auth = paste('Bearer', authToken, sep=' ')
+  h$reset()
+  RCurl::curlPerform(url = tUrl,
+              httpheader=c('Authorization' = auth, 'Content-Type' = "application/json", 'Accept' = "application/json"),
+              writefunction = h$update,
+              headerfunction = hdr$update,
+              verbose = TRUE
+  )
+
+  # Print results
+  return(rjson::fromJSON(h$value()))
+}
+
+
+
+#############################################################
+#' Get a list of webservices available to a workspace
+#'
+#' @param wkID The workspace ID
+#' @param authToken The primary authorization token
+#' @return Returns a list of lists, where each web service is represented
+#' as a nested named list with the following fields:
+#' "Id", "Name", "Description", "CreationTime", "WorkspaceId", "DefaultEndpointName"
+#' @examples
+#' DELETE TOKENS IN REAL VERSION
+#' services = getWebServices("c01fb89129aa4ef0a19affa7f95ecbbc", "523709d06661441bbf129d68f84cd6a4")
+#' serviceID = services[[1]]["Id"]
+#############################################################
+getWebServices <- function(wkID, authToken, url=wsURL) {
+  return(getFramework(sprintf(url, wkID), authToken))
+}
+
+
+
+#############################################################
+#' Get detailed information about a specific webservice
+#'
+#' @param wkID The workspace ID
+#' @param authToken The primary authorization token
+#' @param wsID The webservice ID
+#' @return Returns a named list representing the web service
+#' with the following fields:
+#' "Id", "Name", "Description", "CreationTime", "WorkspaceId", "DefaultEndpointName"
+#' @examples
+#' DELETE TOKENS IN REAL VERSION
+#' service = getWSDetails("c01fb89129aa4ef0a19affa7f95ecbbc", "523709d06661441bbf129d68f84cd6a4", "6a46d1f2a5e6406b8b1a5c563bf1cd10")
+#' serviceID = service["Id"]
+#############################################################
+getWSDetails <- function(wkID, authToken, wsID, url=wsURLdet) {
+  return(getFramework(sprintf(url, wkID, wsID), authToken))
+}
+
+
+
+#############################################################
+#' Get the endpoints that are part of a web service
+#'
+#' @param wkID The workspace ID
+#' @param authToken The primary authorization token
+#' @param wsID The webservice ID
+#' @return Returns a list of lists, where each endpoint is represented
+#' as a nested named list with the following fields:
+#' "Name", "Description", "CreationTime", "WorkspaceId", "WebServiceId",
+#' "HelpLocation", "PrimaryKey", "SecondaryKey", "ApiLocation", "Version",
+#' "MaxConcurrentCalls", "DiagnosticsTraceLevel", "ThrottleLevel"
+#' @examples
+#' DELETE TOKENS IN REAL VERSION
+#' endpoints = getEndpoints("c01fb89129aa4ef0a19affa7f95ecbbc", "523709d06661441bbf129d68f84cd6a4", "6a46d1f2a5e6406b8b1a5c563bf1cd10")
+#' defaultEP = endpoints[[1]]
+#############################################################
+getEndpoints <- function(wkID, authToken, wsID, url=epURL) {
+  return(getFramework(sprintf(url, wkID, wsID), authToken))
+}
+
+
+
+#############################################################
+#' Get the details on a specific endpoint
+#'
+#' @param wkID The workspace ID
+#' @param authToken The primary authorization token
+#' @param wsID The webservice ID
+#' @param epID The endpoint name
+#' @return Returns a named list representing the endpoint with the following fields:
+#' "Name", "Description", "CreationTime", "WorkspaceId", "WebServiceId",
+#' "HelpLocation", "PrimaryKey", "SecondaryKey", "ApiLocation", "Version",
+#' "MaxConcurrentCalls", "DiagnosticsTraceLevel", "ThrottleLevel"
+#' @examples
+#' DELETE TOKENS IN REAL VERSION
+#' defaultEP = getEPDetails("c01fb89129aa4ef0a19affa7f95ecbbc", "523709d06661441bbf129d68f84cd6a4", "6a46d1f2a5e6406b8b1a5c563bf1cd10", "default")
+#############################################################
+getEPDetails <- function(wkID, authToken, wsID, epID, url=epURLdet) {
+  return(getFramework(sprintf(url, wkID, wsID, epID), authToken))
+}
---
+++ maml/R/publish.R
@@ -0,0 +1,327 @@
+# strings for navigating
+publishURL <- "https://hiteshsm.cloudapp.net/workspaces/%s/webservices/%s" ## REMOVE SSL IGNORING FOR REAL VERSION ##
+wrapper <- "inputDF <- maml.mapInputPort(1)\r\noutputDF <- data.frame(matrix(ncol = %s, nrow = nrow(inputDF)))\r\nfor (file in list.files(\"src\")) {\r\n  if (file == \"%s\") {\r\n    load(\"src/%s\")\r\n    for (item in names(dependencies)) {\r\n      assign(item, dependencies[[item]])\r\n    }\r\n  }\r\n  else {\r\n    install.packages(paste(\"src\", file, sep=\"/\"), repos=NULL, type=\"source\")\r\n    library(file, character.only=TRUE)\r\n  }\r\n}\r\naction <- %s\r\n  for (i in 1:nrow(inputDF)) {\r\n    outputDF[i,] <- do.call(\"action\", as.list(inputDF[i,]))\r\n  }\r\nmaml.mapOutputPort(\"outputDF\")"
+
+##################################################################################
+# WRAPPER TESTING
+# Consider: assert statements (stopifnot), error handling
+# wrap in a function that will return a string with the proper function
+##################################################################################
+# get the input
+inputDF <- maml.mapInputPort(1)
+
+# initialize an empty output dataframe of desired dimensions
+outputDF <- data.frame(matrix(ncol = outVals, nrow = nrow(inputDF)))
+
+"inputDF <- maml.mapInputPort(1)\r\noutputDF <- data.frame(list.files(\"src\"))\r\nmaml.mapOutputPort(\"outputDF\")"
+
+for (file in list.files("src")) {
+  if (file == FILENAME) {
+    load("src/FILENAME")
+    # assert that dependencies exists?
+    # NOTE: depedencies object comes from packDependencies(), maybe something more unique to avoid collisions?
+    for (item in names(dependencies)) {
+      assign(item, dependencies[[item]])
+    }
+  }
+  else {
+    # Realized that you can't just zip packages and install them, need source
+    # Currently just planning on installing from CRAN, solves dependency problem
+    # but what if users want to use a package not on CRAN?
+
+    #install.packages(paste("src", file, sep="/"), repos=NULL, type="source")
+    # if the package isn't installed on Azure already, install it and its dependencies
+    if (!(file %in% installed.packages()[,"Package"])) {
+      install.packages(file, dependencies=TRUE)
+    }
+    # load the package
+    library(file, character.only=TRUE)
+  }
+}
+
+# user function
+action <-
+
+  # apply function to every row
+  for (i in 1:nrow(inputDF)) {
+    outputDF[i,] <- do.call("action", as.list(inputDF[i,]))
+  }
+
+# return output
+maml.mapOutputPort("outputDF")
+
+
+"inputDF <- maml.mapInputPort(1)\r\noutputDF <- data.frame(matrix(ncol = outVals, nrow = nrow(inputDF)))\r\nfor (file in list.files(\"src\")) {\r\n  if (file == \"dependencies\") {\r\n    load(\"src/dependencies\")\r\n    for (item in names(dependencies)) {\r\n      assign(item, dependencies[[item]])\r\n    }\r\n  }\r\n  else {\r\n    install.packages(paste(\"src\", file, sep=\"/\"), repos=NULL, type=\"source\")\r\n    library(file, character.only=TRUE)\r\n  }\r\n}\r\naction <- %s\r\n  for (i in 1:nrow(inputDF)) {\r\n    outputDF[i,] <- do.call(\"action\", as.list(inputDF[i,]))\r\n  }\r\nmaml.mapOutputPort(\"outputDF\")"
+
+add <- function(x) {
+  print(findGlobals(add))
+  return(x+a[[1]])
+}
+
+
+################################################################
+# GET THE FUNCTION SOURCE CODE AS A STRING
+# Return the function as a string
+# Also consider paste(body(fun())) or getAnywhere()
+################################################################
+getFunctionString <- function (x)
+{
+  if (tryCatch(!is.character(x), error = function(e) TRUE))
+    x <- as.character(substitute(x))
+  objs <- list()
+  where <- character()
+  visible <- logical()
+  if (length(pos <- find(x, numeric = TRUE))) {
+    objs <- lapply(pos, function(pos, x) get(x, pos = pos),
+                   x = x)
+    where <- names(pos)
+    visible <- rep.int(TRUE, length(pos))
+  }
+  if (length(grep(".", x, fixed = TRUE))) {
+    np <- length(parts <- strsplit(x, ".", fixed = TRUE)[[1L]])
+    for (i in 2:np) {
+      gen <- paste(parts[1L:(i - 1)], collapse = ".")
+      cl <- paste(parts[i:np], collapse = ".")
+      if (gen == "" || cl == "")
+        next
+      Call <- substitute(getS3method(gen, cl, TRUE), list(gen = gen,
+                                                          cl = cl))
+      f <- eval.parent(Call)
+      if (!is.null(f) && !is.null(environment(f))) {
+        ev <- topenv(environment(f), baseenv())
+        nmev <- if (isNamespace(ev))
+          getNamespaceName(ev)
+        else NULL
+        objs <- c(objs, f)
+        msg <- paste("registered S3 method for", gen)
+        if (!is.null(nmev))
+          msg <- paste(msg, "from namespace", nmev)
+        where <- c(where, msg)
+        visible <- c(visible, FALSE)
+      }
+    }
+  }
+  for (i in loadedNamespaces()) {
+    ns <- asNamespace(i)
+    if (exists(x, envir = ns, inherits = FALSE)) {
+      f <- get(x, envir = ns, inherits = FALSE)
+      objs <- c(objs, f)
+      where <- c(where, paste("namespace", i, sep = ":"))
+      visible <- c(visible, FALSE)
+    }
+  }
+  ln <- length(objs)
+  dups <- rep.int(FALSE, ln)
+  if (ln > 1L)
+    for (i in 2L:ln) for (j in 1L:(i - 1L)) if (identical(objs[[i]],
+                                                          objs[[j]], ignore.environment = TRUE)) {
+      dups[i] <- TRUE
+      break
+    }
+  res <- list(name = x, objs = objs, where = where, visible = visible,
+              dups = dups)
+  class(res) <- "getAnywhere"
+
+  #don't show the full response!
+  #res
+  return(gsub("\n", "\r\n", gsub("\"", "\\\"", objs)))
+}
+
+
+
+##################################################################################
+# EXTRACT DEPENDENCIES
+# Takes closure, not string
+##################################################################################
+packDependencies <- function(funName) {
+  dependencies = list()
+  packages = list()
+
+  # generate a GUID to act as a file name to store packages, R data
+  guid = gsub("-", "", uuid::UUIDgenerate(use.time=TRUE))
+
+  # NOTE: will not work if the user function specifies the names directly, e.g. won't find rjson::toJSON
+  # from findGlobals man page: "R semantics only allow variables that might be local to be identified"
+  # CONSIDER: how robust is this filtering? need to verify
+  for (obj in codetools::findGlobals(get(funName))) {
+    name = get(obj)
+
+    # filter out primitives
+    if (is.primitive(name)) {
+        next
+    }
+
+    # get objects
+    else if (!is.function(name)) {
+      dependencies[[obj]] <- name
+    }
+
+    # grab user defined functions
+    else if (identical(environment(name), globalenv())) {
+      dependencies[[obj]] <- name
+    }
+
+    # get the names of packages of package functions
+    else if (paste(getNamespaceName(environment(name))) != "base") {
+      packages[[obj]] <- getNamespaceName(environment(name))
+    }
+
+    # need an else branch?
+  }
+
+  # save current path to restore to later
+  start = getwd()
+  # go to package library, doing this to prevent tarballing entire path to package
+  # TODO: what if packages are in different library directories? need to iterate through all paths
+  setwd(.libPaths()[[1]])
+  # pack up each package in its own tarball (tz)
+  for (pkg in packages) {
+    # should error handle, e.g. if can't find package
+    tar(paste(start, paste(pkg, "tar", sep="."), sep="/"), pkg)
+  }
+  # go back to where the user started
+  setwd(start)
+  # list of things to include in .zip
+  toZip = vector()
+  # gunzip stuff (gz)
+  for (pkg in packages) {
+    R.utils::gzip(paste(pkg, "tar", sep="."), destname=paste(pkg, "tar", "gz", sep="."), overwrite=TRUE)
+
+    toZip <- c(toZip, paste(pkg, "tar", "gz", sep="."))
+  }
+
+  # objects, functions, etc.
+  if (length(dependencies) > 0) {
+    # maybe can save directly as a .zip and skip the zip() call?
+    save(dependencies, file=guid)
+    toZip <- c(toZip, guid)
+  }
+
+  # zip up everything
+  if (length(toZip) > 0) {
+    zip(zipfile=guid, files=toZip)
+    zipEnc <- base64enc::base64encode(paste(guid, ".zip", sep=""))
+
+    # delete the packages
+    for (pkg in packages) {
+      # did I miss anything? maybe extra files floating around
+      file.remove(paste(pkg, "tar", "gz", sep="."))
+    }
+    # delete the dependency rdta file
+    file.remove(guid)
+    file.remove(paste(guid,"zip",sep="."))
+
+    # return the encoded zip as a string
+    return(list(guid, zipEnc))
+  }
+  # if nothing was zipped, return false
+  else {
+    return(list(guid, FALSE))
+  }
+}
+
+
+
+################################################################
+# CONVERT FORMAT
+# Helper function to convert expected schema to API-expecting format
+################################################################
+convert <- function(argList) {
+  form <- list()
+  for (arg in names(argList)) {
+    type = argList[[arg]]
+    # probably a better way to code this
+    if (type == "float" || type == "double") {
+      form[[ arg ]] <- list("type"="number", "format"=type)
+    }
+    else if (type == "date-time" || type == "string" || type == "time-span") {
+      form[[arg]] <- list("type"="string", "format"=type)
+    }
+    else if (type == "uint16" || type == "int16" || type == "uint32" || type == "int32" || type == "uint64" || type == "int64") {
+      form[[arg]] <- list("type"="integer", "format"=type)
+    }
+    else if (type == "int") {
+      form[[arg]] <- list("type"="integer", "format"="int32")
+    }
+    else if (type == "bool" || type == "boolean") {
+      form[[arg]] <- list("type"="boolean")
+    }
+    else {
+      stop(sprintf("Error: data type \"%s\" not supported", type), call. = TRUE)
+    }
+  }
+  return(form)
+}
+
+
+
+################################################################
+# expecting inputSchema = list("arg1"="type", "arg2"="type", ...)
+# expecting outputSchema = list("output1"="type", "output2"="type", ...)
+# funName is a string!!
+################################################################
+# TODO: play around with argument order
+publishWebService <- function(funName, serviceName, inputSchema, outputSchema, wkID, authToken) {
+
+  # Accept SSL certificates issued by public Certificate Authorities
+  options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
+
+  # Get and encode the dependencies
+  zipString = packDependencies(funName)
+
+  # Build the body of the request, differing on whether or not there is a zip to upload
+  # Probably a more elegant way to do this
+  if (zipString[[2]] == FALSE) {
+    req = list(
+      "Name" = serviceName,
+      "Type" = "Code",
+      "CodeBundle" = list(
+        "InputSchema" = convert(inputSchema),
+        "OutputSchema" = convert(outputSchema),
+        "Language" = "r-3.1-64",
+        "SourceCode" = sprintf(wrapper, length(outputSchema), zipString[[1]], zipString[[1]], paste(getFunctionString(funName)))
+
+      )
+    )
+  }
+  else {
+    req = list(
+      "Name" = serviceName,
+      "Type" = "Code",
+      "CodeBundle" = list(
+        "InputSchema" = format(inputSchema),
+        "OutputSchema" = format(outputSchema),
+        "Language" = "r-3.1-64",
+#        "SourceCode" = sprintf(wrapper, length(outputSchema), zipString[[1]], zipString[[1]], paste(getFunctionString(funName))),
+        "SourceCode" = "inputDF <- maml.mapInputPort(1)\r\ninstall.packages(paste(\"src\", \"codetools.zip\", sep=\"/\"), lib = \".\", repos=NULL)\r\nlibrary(\"codetools\")\r\noutputDF <- data.frame(findGlobals(findGlobals))\r\nmaml.mapOutputPort(\"outputDF\")",
+#        "SourceCode" = "inputDF <- maml.mapInputPort(1)\r\noutputDF <- data.frame(list.files(\"src\")\r\nmaml.mapOutputPort(\"outputDF\")",
+        "ZipContents" = tZip # zipString
+      )
+    )
+  }
+
+  # convert the payload to JSON as expected by API
+  # TODO: consolidate json packages, i.e. use only one if possible
+  print(req)
+  body = RJSONIO::toJSON(req)
+
+  # Response gatherer
+  h = RCurl::basicTextGatherer()
+  h$reset()
+
+  # Generate unique guid
+  guid = gsub("-", "", uuid::UUIDgenerate(use.time=TRUE))
+
+  # API call
+  RCurl::httpPUT(url = sprintf(publishURL, wkID, guid), # defined above
+                 httpheader=c('Authorization' = paste('Bearer', authToken, sep=' '),
+                          'Content-Type' = 'application/json',
+                          'Accept' = 'application/json'),
+                 content = body,
+                 writefunction = h$update,
+                 ssl.verifyhost = FALSE) ### REMOVE THIS FOR THE REAL VERSION ###
+
+  # return everything
+  # TODO: format output
+  rjson::fromJSON(h$value())
+}
