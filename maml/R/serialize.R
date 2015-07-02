b5645c73d46731e08b19ff8fdccb856676445ff1
Alex Wang <alexwang@college.harvard.edu>
2015-07-02 14:48:11 -0700

    renaming Serialization to serialize.R to be more in line with current naming conventions

--- maml/R/serialize.R
+++ maml/R/serialize.R
@@ -23,7 +23,7 @@ publishedFunction <<- getFunctionString
 #' inputSchema = list("arg1"="type", "arg2"="type", ...)}
 #' }
 #' \value {The serialized input paramaters.}
-#' 
+#'
 #' Create a DataFrame with these arguments
 #' ***Need to make dataframe global***
 #' Expecting:
@@ -34,10 +34,10 @@ serializeI <- function(input) {
   #convert input to vectors to be added to dataframe
   inArgs <<- unlist(input)
   inputDF <<- data.frame(inArgs)
-  
+
   #serialize data
   sArgs = jsonlite::serializeJSON(inArgs)
-  
+
   return(sArgs)
 }
 
@@ -52,7 +52,7 @@ serializeI <- function(input) {
 #' outputSchema = list("arg1"="type", "arg2"="type", ...)}
 #' }
 #' \value {The serialized output paramaters.}
-#' 
+#'
 #' Create a DataFrame with these arguments
 #' ***Need to make dataframe global***
 #' Expecting:
@@ -63,7 +63,7 @@ serializeO <- function(output) {
   #convert output to vectors to be added to dataframe
   outArgs <<- unlist(output)
   outputDF <<- data.frame(outArgs)
-  
+
   #serialize data
   oArgs <- jsonlite::serializeJSON(outArgs)
   return (oArgs)
@@ -80,7 +80,7 @@ serializeO <- function(output) {
 #' outputSchema = list("arg1"="type", "arg2"="type", ...)}
 #' }
 #' \value {The serialized function body}
-#' 
+#'
 #' Serialized Body of Function
 #' Change function to accept a dataframe
 #' @param function
@@ -88,7 +88,7 @@ serializeO <- function(output) {
 serializeFunc <- function(publishedFunction) {
   # serialize body of function
   bodyFunction <- body(publishedFunction)
-  
+
   return (jsonlite::serializeJSON(bodyFunction))
 }
 
@@ -103,7 +103,7 @@ serializeFunc <- function(publishedFunction) {
 #' \item{functionName}{Name of the published function to run}
 #' }
 #' \value {The serialized ouput}
-#' 
+#'
 #' Serialize (Published) User Arguments: Expect arguments from user in a dataframe
 #' Take Output from function and save into output dataframe
 #' @param Pass dataframe into function
@@ -111,12 +111,12 @@ serializeFunc <- function(publishedFunction) {
 serializeMeta <- function(userDF, funcName) {
   # convert back to params
   args <- as.list(userDF[,])
-  
+
   #run function
   output <- do.call(funcName, args)
   print(output)
   returnDF <- data.frame(output)
-  
+
   #serialize output
   oArgs = jsonlite::serializeJSON(returnDF)
   return (oArgs)
@@ -132,7 +132,7 @@ serializeMeta <- function(userDF, funcName) {
 #' \item{output}{The output from the function run}
 #' }
 #' \value {The unserialized ouput}
-#' 
+#'
 #' Serialize (Published) Output: This is an internal funciton
 #' Take Output from function and save into output dataframe
 #' @param Pass dataframe back to user
@@ -141,7 +141,7 @@ unserializeMeta <- function(output) {
   # convert back to params
   outputDF <<- jsonlite::unserializeJSON(output)
   outlist <- as.list(outputDF[,])
-  
+
   print(outlist)
   return (outlist)
 }
