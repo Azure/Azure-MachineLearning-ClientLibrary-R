b1b8b5bcf4e261e64283b4b81f2cc924f638007b
Alex Wang <alexwang@college.harvard.edu>
2015-07-20 17:00:10 -0700

    forgot to add new folder on previous commit

---
+++ test/Titanic.R
@@ -0,0 +1,74 @@
+test <- read.csv(file="test.csv")
+train <- read.csv(file="train.csv")
+
+head(test)
+head(train)
+
+#y variable
+survived <- train$Survived
+
+#id 
+passengerId <- test$PassengerId
+
+#remove from the training sample
+train = train[,-2]
+
+end_trn = nrow(train)
+
+#combine the two sets
+train <- rbind(train, test)
+#Age replace with mean 
+train$Age[is.na(train$Age)] <- 30 
+end = nrow(train)
+
+#remove columns
+train = train[,c(-1,-3,-8,-10,-11)]
+head(train)
+
+library(gbm)
+set.seed(123)
+pr=0
+tr=0
+
+n.models = 5
+ntrees=2000
+
+for(i in 1:n.models){
+  GBM.model2 = gbm.fit(
+    x=train[1:end_trn,], y = survived,
+    distribution= "gaussian",
+    n.trees = ntrees,
+    shrinkage = 0.01,
+    interaction.depth = 25,
+    n.minobsinnode = 5,
+    verbose = TRUE)
+  #test set prediction
+  pr1 = predict(object=GBM.model2,newdata=train[(end_trn+1):end,], ntrees)
+  #training set prediction
+  tr1 = predict(object = GBM.model2,newdata=train[1:end_trn,], ntrees)
+  pr = pr+pr1
+  tr = tr+tr1
+}
+pr = pr/n.models
+tr = tr/n.models
+head(pr)
+head(tr)
+summary(GBM.model2)
+
+#round the preidctions
+pr = round(pr)
+tr = round(tr)
+
+#compare results
+head(tr, n=20)
+head(survived, n = 20)
+
+#in sample classification accuracy
+1 - sum(abs(survived - tr)) / nrow(train)
+
+predictTitanic2 <- function (Pclass, Sex, Age, SibSp, Parch, Fare) {
+  return(predict.gbm(object=GBM.model2, newdata=data.frame("Pclass"=Pclass, "Sex"=Sex, "Age"=Age, "SibSp"=SibSp, "Parch"=Parch, "Fare"=Fare), 2000))
+}
+
+submission = data.frame(PassengerId = passengerId, survived = pr)
+write.csv(submission, file = "gbm submission.csv", row.names = FALSE)
---
+++ test/iris.R
@@ -0,0 +1,32 @@
+library(e1071)
+
+setwd("C://Users/t-alewa/Documents/Azure-MachineLearning-ClientLibrary-R/test")
+# t-alewa on studio internal
+wsID = "3612640f27234eb7b2b91ac62e8b4a40"
+wsAuth = "abcbe14a958a40978f93aa0e0e71f5be"
+
+dataset <- read.csv(file="iris.csv")
+
+features <- get.feature.columns(dataset)
+labels   <- as.factor(get.label.column(dataset))
+
+train.data <- data.frame(features, labels)
+feature.names <- get.feature.column.names(dataset)
+names(train.data) <- c(feature.names, "Class")
+
+#model <- naiveBayes(Class ~ ., train.data)
+model <- naiveBayes(as.factor(Species) ~., dataset)
+
+probabilities <- predict(model, dataset, type="raw")[,2]
+#classes <- as.factor(as.numeric(probabilities >= 0.5))
+classes <- as.factor(predict(model, dataset))
+scores <- data.frame(classes, probabilities)
+
+predictClass <- function(sepalLength, sepalWidth, petalLength, petalWidth) {
+  predictDF <- predict(model, data.frame("sepalLength" = sepalLength, "sepalWidth" = sepalWidth, "petalLength" = petalLength, "petalWidth"=petalWidth), type="raw")
+  return(colnames(predictDF)[apply(predictDF, 1, which.max)])
+}
+
+irisService <- publishWebService("predictClass", "irisService", list("sepalLength"="float", "sepalWidth"="float", "petalLength"="float", "petalWidth"="float"), list("class"="int"), wsID, wsAuth)
+endpoints <- irisService[[2]]
+response <- consumeDataTable(endpoints[[1]]["PrimaryKey"], paste(endpoints[[1]]["ApiLocation"], "/execute?api-version=2.0&details=true",sep=""), list("sepalLength", "sepalWidth", "petalLength", "petalWidth"), list(5, 5, 4, 3), list(4.5, 6.5, 4.5, 2))
